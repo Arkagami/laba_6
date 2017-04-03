333986095e0b7a661972ced23fee6080e0b6fcda
Arkagami <mixno20@gmail.com>
2017-04-03 15:33:01 +0300

    Начал шифрование

--- Source.c/Source.c/Source.cpp
+++ Source.c/Source.c/Source.cpp
@@ -1,25 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <math.h>
+
+
+char cipher(char c, int i) {
+	return 0;
+}
 
 
 int main()
 {
 	FILE* fin;
+	FILE* fcip;
 	FILE* fout;
-
 	fin = fopen("Mixno.bmp", "rb");
+	fcip = fopen("input.txt", "r");
 	fout = fopen("Out.bmp", "wb");
-	char c = 0;
+	char c = 0, cip = 0;
+	int can = 0;
 	for (int i = 0;i < 54;i++) {
 		c = fgetc(fin); fputc(c, fout);
 	}
 	while (1) {
-		c = fgetc(fin); 
-		if (feof(fin)) break; 
+		c = fgetc(fin);
+		if(can%=8,can==0) cip = fgetc(fcip);
+		can++;
+		c = cipher(c, cip / int(pow(2.0, 8.0 - double(can))));
+		if (feof(fin)) break;
+		if (feof(fcip)) cip = '#';
+
 		fputc(c, fout);
 	}
 
 	fclose(fin);
+	fclose(fcip);
 	fclose(fout);
 	return 0;
 }
