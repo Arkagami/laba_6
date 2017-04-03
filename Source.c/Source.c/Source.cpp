499b97308e93f1687a62d8b6d8d7b4cf0e259b1b
Arkagami <mixno20@gmail.com>
2017-04-03 17:26:02 +0300

    Ну хз хз...

--- Source.c/Source.c/Source.cpp
+++ Source.c/Source.c/Source.cpp
@@ -6,44 +6,31 @@
 int main()
 {
 	FILE* fin;
-	FILE* fcip;
 	FILE* fout;
-	fin = fopen("Mixno.bmp", "rb");
-	fcip = fopen("input.txt", "r");
-	fout = fopen("Out.bmp", "wb");
+	fin = fopen("Out.bmp", "rb");
+	fout = fopen("output.txt", "w");
 	unsigned char c = 0, cip = 0;
 	int can = 0, cipher[8];
 	for (int i = 0;i < 54;i++) {
-		c = fgetc(fin); fputc(c, fout);
+		c = fgetc(fin);
+	}
+	while (1) {
+		cip = 0;
+			for (int i = 7; i >= 0; i--) {
+				c = fgetc(fin);
+				cip = (cip * 2) + (c % 2);
+			}
+
+			if (cip == '#') break;
+		fputc(cip, fout);
 	}
 	while (1) {
 		c = fgetc(fin);
 		if (feof(fin)) break;
-		if (feof(fcip)) {
-			cip = '#'; goto suda;
-		}
-		if (can %= 8, can == 0) {
-			cip = fgetc(fcip);
-		suda:;
-			if(cip == '#') goto tuda;
-			for (int i = 7; i >= 0; i--) {
-				cipher[i] = cip % 2;
-				cip /= 2;
-			}
-		}
-		can++;
-		if (feof(fcip)) { 
-			fputc(c, fout); 
-			continue; 
-		}
-	tuda:;
-		c /= 2;
-		c *= 2;
-		fputc((c+cipher[8-can]), fout);
+		fputc(c, fout);
 	}
 
 	fclose(fin);
-	fclose(fcip);
 	fclose(fout);
 	return 0;
 }
