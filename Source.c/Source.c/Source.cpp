d7927dd34e4d6b6c37b551effee725206cd5c3f2
Arkagami <mixno20@gmail.com>
2017-04-03 18:35:27 +0300

    hz

--- Source.c/Source.c/Source.cpp
+++ Source.c/Source.c/Source.cpp
@@ -1,36 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
 
 
 int main()
 {
-	FILE* fin;
-	FILE* fout;
-	fin = fopen("Out.bmp", "rb");
-	fout = fopen("output.txt", "w");
-	unsigned char c = 0, cip = 0;
-	int can = 0, cipher[8];
-	for (int i = 0;i < 54;i++) {
-		c = fgetc(fin);
+	int i = 0;
+	char *setNames[5], *word;
+	word = "namilonhghjgghj";
+	setNames[0] = "namilon";
+	while (*setNames[i] == *word) {
+		printf("%c %c\n", *setNames[i]++,*word++);
+		if (*setNames[i] == '\0') printf("%s\n", word);
 	}
-	while (1) {
-		cip = 0;
-			for (int i = 0; i < 8; i++) {
-				c = fgetc(fin);
-				cip = (cip * 2) + (c % 2);
-			}
-			if (feof(fin)) break;
-			if (cip == '#') break;
-		fputc(cip, fout);
-	}
-	while (1) {
-		c = fgetc(fin);
-		if (feof(fin)) break;
-		fputc(c, fout);
-	}
-
-	fclose(fin);
-	fclose(fout);
 	return 0;
 }
